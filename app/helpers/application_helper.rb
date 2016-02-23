557f96f7de0875040abaa4f10655ee419d1c889e
NESL <nesl.legacy@gmail.com>
2016-02-22 17:16:49 -0800

    Fix incorrect url checking with res. code 302
    Previously, url_exists?() returns false if the foreign server returns
    302 (redirection.) In new implementation, we trace the redirected
    url with a certain trial.
    
    The implementation is based on http://stackoverflow.com/questions/5908017/check-if-url-exists-in-ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,10 @@ module ApplicationHelper
 
   # NOTE: This function can block the rest of procedures. Consider reimplement
   # this in javascript
-  def url_exists?(url_string)
+  def url_exists?(url_string, trial = 3)
+    if trial <= 0
+      false
+    end
     url = URI.parse(url_string)
     req = Net::HTTP.new(url.host, url.port)
     req.open_timeout = 1
@@ -23,7 +26,11 @@ module ApplicationHelper
     req.use_ssl = (url.scheme == "https")
     path = url.path.present? ? url.path : '/'
     res = req.request_head(path)
-    res.code == "200"
+    if res.kind_of?(Net::HTTPRedirection)
+      url_exists?(res['location'])
+    else
+      ! %W(4 5).include?(res.code[0])
+    end
   rescue
     false #can't find the server
   end
