be4551e8a8f8f8fb61ba829a1ee20f9e73ef9cf5
timestring <timestringalpha@gmail.com>
2016-02-25 17:32:29 -0800

    Updated configs for production server

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,14 +7,33 @@ working_directory app_dir
 # Set unicorn options
 worker_processes 4
 preload_app true
-timeout 300
+timeout 30
 
 # Set up socket location
 listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 
 # Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+# stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+# stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
 pid "#{shared_dir}/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
