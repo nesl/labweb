b1fc387b6dbf5bc813a894123eaf351e06b86eb0
timestring <timestringalpha@gmail.com>
2016-03-02 16:01:21 -0800

    Production setup

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -13,8 +13,8 @@ timeout 30
 listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 
 # Logging
-# stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-# stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
 pid "#{shared_dir}/pids/unicorn.pid"
