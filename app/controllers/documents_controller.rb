4b6f5033d3c0bca74c765111e8a20277ce7bb1e6
timestring <timestringalpha@gmail.com>
2016-04-28 15:53:58 -0700

    Add TODO for future optimization

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,8 +6,9 @@ class DocumentsController < ApplicationController
   def index
     # TODO: if the document category or main research area are not chosen,
     # the form validator will pass, but it will crash here
+    # TODO: more optimization: http://stackoverflow.com/questions/12400860/activerecord-includes-specify-included-columns
     @documents = Document.includes(:people).all.order('pubdate DESC')
-    
+
     Rails.logger.debug(@documents.to_sql)
     
     @document_categories = DocumentCategory.all.select{ |dc|
